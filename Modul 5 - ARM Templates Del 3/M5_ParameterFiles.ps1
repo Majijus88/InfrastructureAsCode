31cc023dc0afa6832f14fd0fe2fbfa6cdae2bbc9
Majijus88 <marius.holtmoen@gmail.com>
2022-12-08 20:14:31 +0100

    Deployer ved hjelp av parameterfil til templaten

---
+++ Modul 5 - ARM Templates Del 3/M5_ParameterFiles.ps1
@@ -0,0 +1,21 @@
+#$tenantid = "02feabb9-444e-4f66-9c13-6a8f04b75c2f"
+#$subscriptionid = "efc1e7b1-5729-4eea-b33e-12cc6b1c0183"
+#$credentials = Get-Credential
+#$userID = "34c49619-211d-4383-bc27-7ec888f28c8f"
+#$secret = "pa08Q~n4Ps2h5blo8Y6UxMJ~vl.SROCiCCbjsc8s"
+#Connect-AzAccount -ServicePrincipal -Credential $credentials -TenantId $tenantid -Environment AzureCloud -Subscription $subscriptionid
+
+$rg = "Modul-05-Oving"
+$location = "North Europe"
+#Oppretter en ny Resource Group med navn og location definert ovenfor i variabelene $rg og $location
+New-AzResourceGroup -Name $rg -Location $location -Force
+
+#Oppretter en ny deployment som henter verdier til parametere fra parameterfilen.
+New-AzResourceGroupDeployment `
+    -Name "M5-ParameterFiles" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\Modul 5 - ARM Templates Del 3\M5_ParameterFiles.json' `
+    -TemplateParameterFile '.\Modul 5 - ARM Templates Del 3\M5_ParameterFiles.parameters.json'
+
+#Kommandoen nedenfor brukes for å slette RG jeg benytter i modulen
+#Remove-AzResourceGroup -Name $rg -Force:$true
