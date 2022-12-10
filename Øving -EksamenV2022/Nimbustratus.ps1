a081dc9f4027667afd4f9fa7ddf4353dd36b0544
Majijus88 <marius.holtmoen@gmail.com>
2022-12-11 00:18:09 +0100

    Commit Nr.1
    Opprettet ARM-Templaten "Nimbustratus.json" som oppretter følgende
    3x Storage Accounts ved hjelp av CopyLoop
    1x ServerFarm
    1x WebApp med avhengigheter mot ServerFarm og CopyLoop
    Lager nøkler for tilgang til StorageAccountene med listKeys
    
    Laget 3x Parameterfiler:
            1 til dev-miljø,
            1 til prod-miljø
            1 til test-miljø
    
    MÅ GJØRES:
    
    Sette opp parameter for StorageAccountSKU slik at parameterfilene kan få inn dette
    Sette opp noen conditions

---
+++ Øving -EksamenV2022/Nimbustratus.ps1
@@ -0,0 +1,33 @@
+#$tenantid = "02feabb9-444e-4f66-9c13-6a8f04b75c2f"
+#$subscriptionid = "efc1e7b1-5729-4eea-b33e-12cc6b1c0183"
+#$credentials = Get-Credential
+#$userID = "34c49619-211d-4383-bc27-7ec888f28c8f"
+#$secret = "pa08Q~n4Ps2h5blo8Y6UxMJ~vl.SROCiCCbjsc8s"
+#Connect-AzAccount -ServicePrincipal -Credential $credentials -TenantId $tenantid -Environment AzureCloud -Subscription $subscriptionid
+
+$rg = "Nimbustratus-Eksamen-MDH"
+$location = "West Europe"
+#Oppretter en ny Resource Group med navn og location definert ovenfor i variabelene $rg og $location
+New-AzResourceGroup -Name $rg -Location $location -Force
+
+#Oppretter en ny deployment  der jeg kan sette inn Name, RG og Templatefil
+New-AzResourceGroupDeployment `
+    -Name "Nimbustratus-Test-Test" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\Øving -EksamenV2022\Nimbustratus.json' `
+    -TemplateParameterFile '.\Øving -EksamenV2022\Nimbustratus.parameters-test.json'
+
+New-AzResourceGroupDeployment `
+    -Name "Nimbustratus-Prod-Test" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\Øving -EksamenV2022\Nimbustratus.json' `
+    -TemplateParameterFile '.\Øving -EksamenV2022\Nimbustratus.parameters-prod.json'
+
+New-AzResourceGroupDeployment `
+    -Name "Nimbustratus-Dev-Test" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\Øving -EksamenV2022\Nimbustratus.json' `
+    -TemplateParameterFile '.\Øving -EksamenV2022\Nimbustratus.parameters-dev.json'
+
+#Kommandoen nedenfor brukes for å slette RG jeg benytter i modulen
+#Remove-AzResourceGroup -Name $rg -Force:$true
