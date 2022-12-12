f4ec67d0b7df768a3ba7a84d0418513b0112ce49
Majijus88 <marius.holtmoen@gmail.com>
2022-12-12 13:24:18 +0100

    Commit Nr. 1 Opprettet templaten basert på gammel løst eksamensoppgave basert på Nimbustratus Denne templaten deployer: (med defaultverdier):
    - 3x Storage Accounts
    - 1x Blob Container pr. Storage Account. Satt avhengighet mot Storage Account.
    - 1x ServerFarm
    - 1x WebSite med avhengighet mot ServerFarm og copyloopen til Storage Account. Denne lister også ut nøkler til Storage Accountene
    - 1x SQL server med bryterfunksjon til å bestemme hvorvidt Azure skal godta samtlige IP-adresser
    
    Det gis også output om hva som er URI til de forskjellige Storage Accounts via Output.
    
    Templaten er deployet uten problemer.

---
+++ EKSAMEN/Oppgave2/Oppgave2_AlgorithmComputers.ps1
@@ -0,0 +1,36 @@
+#$tenantid = "02feabb9-444e-4f66-9c13-6a8f04b75c2f"
+#$subscriptionid = "efc1e7b1-5729-4eea-b33e-12cc6b1c0183"
+#$credentials = Get-Credential
+#$userID = "34c49619-211d-4383-bc27-7ec888f28c8f"
+#$secret = "pa08Q~n4Ps2h5blo8Y6UxMJ~vl.SROCiCCbjsc8s"
+#Connect-AzAccount -ServicePrincipal -Credential $credentials -TenantId $tenantid -Environment AzureCloud -Subscription $subscriptionid
+
+$rg = "AlgorithmComputers-Oppgave2-MDH"
+$location = "West Europe"
+#Oppretter en ny Resource Group med navn og location definert ovenfor i variabelene $rg og $location
+New-AzResourceGroup -Name $rg -Location $location -Force
+
+#Oppretter en ny deployment  der jeg kan sette inn Name, RG og Templatefil
+New-AzResourceGroupDeployment `
+    -Name "AC-Testmiljø" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\EKSAMEN\Oppgave2\Oppgave2_AlgorithmComputers.json' `
+    -TemplateParameterFile '.\EKSAMEN\Oppgave2\Oppgave2_AlgorithmComputers.parameters-test.json' `
+    -allowAzureIPs "Yes"
+
+New-AzResourceGroupDeployment `
+    -Name "AC-Prodmiljø" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\EKSAMEN\Oppgave2\Oppgave2_AlgorithmComputers.json' `
+    -TemplateParameterFile '.\EKSAMEN\Oppgave2\Oppgave2_AlgorithmComputers.parameters-prod.json' `
+    -allowAzureIPs "Yes"
+
+New-AzResourceGroupDeployment `
+    -Name "AC-Devmiljø" `
+    -ResourceGroupName $rg `
+    -TemplateFile '.\EKSAMEN\Oppgave2\Oppgave2_AlgorithmComputers.json' `
+    -TemplateParameterFile '.\EKSAMEN\Oppgave2\Oppgave2_AlgorithmComputers.parameters-dev.json' `
+    -allowAzureIPs "Yes"
+
+#Kommandoen nedenfor brukes for å slette RG jeg benytter i modulen
+#Remove-AzResourceGroup -Name $rg -Force:$true
